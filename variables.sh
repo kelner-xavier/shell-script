6e1ba1cdbaccac80af8d6e9ada7557235c83d21e
Kelner Xavier <xavier@pop-os.localdomain>
2021-03-05 11:44:47 -0300

    Env Variables

---
+++ variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#OBS.: As variáveis dever ter esta sintaxe tudo junto caso contrário dará erro
+OLA="Olá, Mundo"
+
+#Para usa-se o char '$' para invocar a variável criada. 
+echo $OLA
+
+#Existem algumas variáveis do sistema que podem ser incocadas também e podem ser de utilidade. 
+#Para listá-las usa-se o comando 'env' ou 'printev'.
+#Ex: 'echo $USER' irá imprimir o seu nome de usuário
+
+
+
